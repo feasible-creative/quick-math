b636b83bbeabd924446c9abedb4bc7c21538d0db
feasible <feasible.creative@gmail.com>
2022-09-06 19:55:39 -0400

    Initial commit - create HTML, CSS, JS files (and link to HTML). Design pseudocode, basic JavaScript

---
+++ script.js
@@ -0,0 +1,22 @@
+// 1. Display rnadom math problem
+// 2. Timer starts at 0:10
+// 3. Input answer
+// 4. If correct, add +1 to score and continue to next question
+// 5. If incorrect [GAME OVER] - option to restart
+
+
+
+function getRandomQuestion(){
+  return randomMath = 1+1;
+}
+
+getRandomQuestion()
+
+
+
+
+const mathQuestion = document.querySelector('#question');
+mathQuestion.textContent = randomMath;
+
+
+
