05a64927041bee475218a5bf1e6e8f5e1ba2445a
feasible <feasible.creative@gmail.com>
2022-09-06 20:17:06 -0400

    update CSS properties, click button now displays math problem with random whole numbers from 0 - 25

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const mathQuestion = document.querySelector('#question');
 
 const button = document.querySelector('#questionButton')
 button.addEventListener('click', function nextQuestion(){
-  return test = mathQuestion.textContent = `${Math.floor(Math.random() * 20).toString()} + ${Math.floor(Math.random() * 20).toString()}`;
+  return test = mathQuestion.textContent = `${Math.floor(Math.random() * 25).toString()} + ${Math.floor(Math.random() * 25).toString()}`;
 
 }
 
